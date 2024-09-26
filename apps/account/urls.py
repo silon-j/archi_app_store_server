12fbade9b4d5ad2e680e3c8adbb4a1b698757cd8
Li Guanda <1274805330@qq.com>
2024-09-26 15:09:24 +0800

    修改路由
    - 路由修改
    - 白名单匹配

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views.account_view import LoginView, RegisterView, ChangePasswordView
 from .views.verify_code_view import RegisterVerifyCode, ChangePasswordVerifyCode
+from .views.admin_get_accounts import AdminGetAllAccounts
+from .views.admin_modify_account import AdminChangeAccountPassword, AdminModifyAccount
+from .views.admin_suspend_account import AdminActivateAccount, AdminSuspendAccount, AdminDeleteAccount
 
 
 urlpatterns = [
@@ -9,4 +12,11 @@ urlpatterns = [
     path('/login', LoginView.as_view(), name='login'),
     path('/register', RegisterView.as_view(), name='register'),
     path('/changepassword', ChangePasswordView.as_view(), name='change_password'),
+    # 管理后台接口
+    path('/admin/all-accounts', AdminGetAllAccounts.as_view(), name='get_all_accounts'),
+    path('/admin/change-password', AdminChangeAccountPassword.as_view(), name='change_password'),
+    path('/admin/modify-account', AdminModifyAccount.as_view(), name='modify_account'),
+    path('/admin/activate-account', AdminActivateAccount.as_view(), name='activate_account'),
+    path('/admin/suspend-account', AdminSuspendAccount.as_view(), name='suspend_account'),
+    path('/admin/delete-account', AdminDeleteAccount.as_view(), name='delete_account'),
 ]
