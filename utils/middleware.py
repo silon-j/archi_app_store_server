0261561821b4b95b3a701d40cfa1dfceab550bc4
Li Guanda <1274805330@qq.com>
2024-10-12 10:58:56 +0800

    Update middleware.py

--- utils/middleware.py
+++ utils/middleware.py
@@ -30,7 +30,7 @@ class AuthenticationMiddleware(MiddlewareMixin):
         account = self.get_current_account(request)
         if account is not None:
             # 用户被禁用或者已经被删除
-            if (not account.is_active) or (account.deleted_at is not None):
+            if not account.is_active:
                 return JsonResponse(error_type=ErrorType.ACCOUNT_DISABLED, status_code=HttpStatus.HTTP_401_UNAUTHORIZED)
             if account.is_active and account.token_expired >= timezone.now():
                 request.account = account
