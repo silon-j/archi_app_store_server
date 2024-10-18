85aac47ec9eaaa9848cb5181959ddff2c7848abb
LucasPoint2 <lucaspoint2@yeah.net>
2024-10-18 17:27:12 +0800

    feat:用户管理分页

--- apps/account/views/admin_get_accounts.py
+++ apps/account/views/admin_get_accounts.py
@@ -5,7 +5,7 @@ from django.db.models.functions import Coalesce
 from apps.account.models import Account
 from apps.plugin.models import Developer, OperationLog
 from libs.boost.parser import Argument, JsonParser
-from libs.boost.http import JsonResponse
+from libs.boost.http import JsonResponse, paginate_data
 from utils.decorators import admin_required
 from const.error import ErrorType
 from typing import List
@@ -15,7 +15,12 @@ class AdminGetAllAccounts(View):
     
     @admin_required
     def get(self, request:HttpRequest):
-        
+        param, error = JsonParser(
+            Argument('current', data_type=int, required=False),
+            Argument('page_size', data_type=int, required=False),
+        ).parse(request.GET)
+        if error:
+            return JsonResponse(error_message=error)
         accounts = Account.objects.all()
         
         # 批量获取所有操作日志的数量，并构建 {account_id: use_count} 映射
@@ -38,19 +43,20 @@ class AdminGetAllAccounts(View):
                 dev_plugin_relation[plugin_version.plugin.name] += 1
             developer_plugin_counts[(dev.username, dev.email)] = len(dev_plugin_relation.keys())
 
-
-        result : List[Account] = [
-            {
-            'id':account.id, 
-            'username':account.username, 
-            'fullname':account.fullname, 
-            'email': account.email, 
-            'can_admin':account.can_admin,
-            'is_super':account.is_super,
-            'is_active':account.is_active,
-            'app_use_count': account_operation_counts.get(account.id, 0),
-            'app_publish_count': developer_plugin_counts.get((account.username, account.email), 0),
-            'last_login':account.last_login,
-            } 
-            for account in accounts]
-        return JsonResponse(result)
+        page_data = paginate_data(accounts, 
+                                  current=param.current if param.current != None else 1, 
+                                  page_size=param.page_size if param.page_size != None else 10, 
+                                  item_handler=lambda account:
+                                  {
+                                    'id':account.id, 
+                                    'username':account.username, 
+                                    'fullname':account.fullname, 
+                                    'email': account.email, 
+                                    'can_admin':account.can_admin,
+                                    'is_super':account.is_super,
+                                    'is_active':account.is_active,
+                                    'app_use_count': account_operation_counts.get(account.id, 0),
+                                    'app_publish_count': developer_plugin_counts.get((account.username, account.email), 0),
+                                    'last_login':account.last_login,
+                                  } )
+        return JsonResponse(page_data)
